932c16bc149a5135cb0335621b96ed2b77d3d081
Ekta-Priya <ektapriya36@gmail.com>
2024-09-04 17:01:06 +0000

    updating myfirst file

---
+++ f.js
@@ -0,0 +1,104 @@
+console.log("hiii")
+// {
+//     var account=34
+//     console.log(account)
+// }
+// account=55
+// let marks=89.5
+// let roll=12
+// console.log(account)
+// console.log(marks)
+// console.log(roll)
+
+// nuber=2 to the power 53    NOTESSSSSSSS
+// bigint                           *
+// boolean=true/false               *
+// String                           *
+// null                             *
+// undefined                        *
+// symbol=unique                    *
+//                                  *
+// object                    NOTESSSSSSSS
+
+// let score="32"
+// let marks
+// console.log(typeof score)
+// console.log(typeof (score))
+// console.log(typeof marks)
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+
+// let marks="56"
+// let score="54as"
+// let valuemarks=Number(marks)
+// console.log(typeof marks)
+// console.log(typeof valuemarks)
+// let valuescore=Boolean(score)
+// console.log(typeof score)
+// console.log(typeof valuescore)
+// console.log(valuescore)
+
+// let marks=90                     OPERATIONS
+// let negamarks=-marks                 *
+// console.log(negamarks)               *
+// console.log(2*3)                     *
+// console.log(4**3)
+// console.log(2/3)
+// console.log(2+3)                     *
+// console.log(2%3)                     *
+
+// let str="ekta"
+// let str1="priya"
+// let str2=str + str1
+// console.log(str2)
+// console.log(+true)
+// console.log(+"")
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log(1+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(1+"2"+2)
+// let game=100
+// let over=50
+// console.log(++game)
+// console.log(++game)
+// console.log(++game)
+// console.log(over++)
+// console.log(over++)
+// console.log(over++)
+
+
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2<1)
+// console.log("1"===1)
+// console.log("2"<1)
+// console.log("20">"1")
+// console.log(null==0)
+// console.log(null<=0)
+// console.log(null>0)
+// console.log(undefined==0)
+// console.log(undefined<=0)
+// console.log(undefined>0)
+
+
+// const score=Symbol('123')
+// const id=Symbol('123')
+// console.log(score==id)
+
+// const heros=["spiderman","batman","padman"];                   FUNTION AND ARRAYS
+// let obj ={                                                               *
+//     name:"ekta",                                                         *
+//     dept:"cse",
+//     roll:35
+// }
+// console.log(obj,heros)
+// console.log(typeof heros)
+// const myfunc=function(){
+//     console.log("hello world!!")
+// }                                                                        *
+// myfunc()                                                                 *
